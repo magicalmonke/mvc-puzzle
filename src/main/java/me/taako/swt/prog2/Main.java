1c964ecfe30e2e93a7c2855785bc4bc30a90a0b5
POGROTH THE HYPED <82341064+magicalmonke@users.noreply.github.com>
2024-06-22 09:05:39 +0200

    Integrate MVC components in Main

--- src/main/java/me/taako/swt/prog2/Main.java
+++ src/main/java/me/taako/swt/prog2/Main.java
@@ -1,7 +1,14 @@
 package me.taako.swt.prog2;
 
+import me.taako.swt.prog2.controller.PuzzleController;
+import me.taako.swt.prog2.model.PuzzleModel;
+import me.taako.swt.prog2.view.PuzzleView;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PuzzleModel model = new PuzzleModel(4, 4);
+        PuzzleView view = new PuzzleView(model);
+        PuzzleController controller = new PuzzleController(model, view);
+        controller.initController();
     }
 }
